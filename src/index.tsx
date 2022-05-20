98c3b47c34f823acfbd0283c67723a26ce25ac8b
Adrian Połubiński <polubis@wp.pl>
2022-05-20 15:56:59 +0200

    Finish

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useState } from "react";
 import { render } from "react-dom";
-import { mergeMap, Subject } from "rxjs";
+import { delay, EMPTY, of, Subject, switchMap, tap } from "rxjs";
 import { ExpressComponent } from "./final/express-component";
 
 import { Start } from "./final/coffee-express-state-machine";
@@ -15,15 +15,59 @@ const useExpress = () => {
   const [state, setState] = useState(Start());
 
   const turnOn = () => {
-    setState((state) => {
-      return state.key === "idle" ? state.PowerOn() : state;
-    });
+    start.next();
   };
 
-  // Klikam po 3 sekundach sie wlaczy
-
   React.useEffect(() => {
-    const sub = start$.pipe().subscribe();
+    const sub = start$
+      .pipe(
+        switchMap(() =>
+          of(EMPTY).pipe(
+            tap(() => {
+              setState((state) =>
+                state.key === "idle" ? state.PowerOn() : state
+              );
+            }),
+            delay(1000),
+            tap(() => {
+              setState((state) =>
+                state.key === "powerOn" ? state.Graining() : state
+              );
+            }),
+            delay(2000),
+            tap(() => {
+              setState((state) =>
+                state.key === "graining" ? state.Warming(80) : state
+              );
+            }),
+            delay(2000),
+            tap(() => {
+              setState((state) =>
+                state.key === "warming" ? state.Making() : state
+              );
+            }),
+            delay(4000),
+            tap(() => {
+              setState((state) =>
+                state.key === "warming" ? state.Making() : state
+              );
+            }),
+            delay(1000),
+            tap(() => {
+              setState((state) =>
+                state.key === "making" ? state.Done() : state
+              );
+            }),
+            delay(1000),
+            tap(() => {
+              setState((state) =>
+                state.key === "done" ? state.Idle() : state
+              );
+            })
+          )
+        )
+      )
+      .subscribe();
 
     return () => {
       sub.unsubscribe();
@@ -40,8 +84,22 @@ function App() {
   const { state, turnOn } = useExpress();
 
   return (
-    <div className="container" onClick={() => turnOn()}>
-      <ExpressComponent />
+    <div className="container">
+      <ExpressComponent
+        arrowRotates={state.key !== "idle" && state.key !== "done"}
+        redDot={state.key === "graining"}
+        yellowDot={state.key === "warming"}
+        greenDot={state.key === "making"}
+        animateCoffee={state.key === "making"}
+      />
+
+      <button
+        className="make-coffee-btn"
+        disabled={state.key !== "idle"}
+        onClick={turnOn}
+      >
+        START
+      </button>
     </div>
   );
 }
